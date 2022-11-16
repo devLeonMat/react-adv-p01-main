075f352a6c5642a3810cacdafc65666635f821fa
Leon Matias <roslin.pineda@thebridge.social>
2022-11-15 22:12:26 -0500

    added new components

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,3 @@
-import {LazyPage1, LazyPage2, LazyPage3} from "../lazyload/pages";
 import {lazy, LazyExoticComponent} from "react";
 
 type JSXComponent = () => JSX.Element;
@@ -10,9 +9,9 @@ interface Route {
     name: string;
 }
 
-const lazy1 = lazy(() => import('../lazyload/pages/LazyPage1'));
-const lazy2 = lazy(() => import('../lazyload/pages/LazyPage2'));
-const lazy3 = lazy(() => import('../lazyload/pages/LazyPage3'));
+const lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1"*/'../lazyload/pages/LazyPage1'));
+const lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2"*/'../lazyload/pages/LazyPage2'));
+const lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3"*/'../lazyload/pages/LazyPage3'));
 
 export const routes: Route[] = [
     {
